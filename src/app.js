a1612569b83c527fa3e9796ed6e768e8c6d23958
Igor3550 <igormeloigormelo@gmail.com>
2022-09-28 16:52:19 -0300

    Initial files upload

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = (process.env.PORT || 4000)
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`listen on port ${PORT}`));
---
+++ src/database.js
@@ -0,0 +1,6 @@
+import dotenv from `dotenv`;
+dotenv.config();
+
+const connection = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
