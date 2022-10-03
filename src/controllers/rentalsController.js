b24e2c44dbf6db78831639dbe1bf47f8b052c740
Igor3550 <igormeloigormelo@gmail.com>
2022-10-03 17:01:44 -0300

    feat /rentals: Delete rental

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -63,7 +63,7 @@ async function createRental (req, res) {
 
     await connection.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1,$2,$3,$4,null,$5,null)`, rental)
 
-    res.send({m:"200 msm"})
+    res.sendStatus(201)
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
@@ -86,7 +86,7 @@ async function returnRental (req, res) {
     }
     let delayFee = delay * parseInt(game.rows[0].pricePerDay);
 
-    await connection.query('UPDATE rentals SET "returnDate" = $1, "delayFee" = $2;', [nowDate, delayFee]);
+    await connection.query('UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE id = $3;', [nowDate, delayFee, rentalExists.id]);
 
     res.sendStatus(200)
 
@@ -96,8 +96,25 @@ async function returnRental (req, res) {
   }
 }
 
+async function deleteRental (req, res) {
+  const rentalId = req.params.id;
+  try {
+    const rental = await connection.query('SELECT * FROM rentals WHERE id = $1;', [rentalId]);
+    if(!rental.rows[0]) return res.sendStatus(404);
+    if(!rental.rows[0].returnDate) return res.sendStatus(400);
+    
+    await connection.query('DELETE FROM rentals WHERE id = $1;', [rentalId])
+
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(500)
+  }
+}
+
 export {
   createRental,
   getRentals,
-  returnRental
+  returnRental,
+  deleteRental
 }
--- src/routers/rentalsRouter.js
+++ src/routers/rentalsRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRental, getRentals, returnRental } from '../controllers/rentalsController.js';
+import { createRental, getRentals, returnRental, deleteRental } from '../controllers/rentalsController.js';
 import { createRentalMiddleware, finishRentalMiddleware } from '../middlewares/rentalMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/rentals', getRentals)
 router.post('/rentals', createRentalMiddleware, createRental)
 router.post('/rentals/:id/return', finishRentalMiddleware, returnRental)
+router.delete('/rentals/:id', deleteRental)
 
 export default router;
